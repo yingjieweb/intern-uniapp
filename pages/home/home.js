44d9c8cd0b72b8bfdeed7e481450459e1fcec873
songyingjie <l>
2020-02-10 08:55:33 +0800

    封装网络请求and首页轮播图

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,22 @@
 // pages/home/home.js
+import { getMultiData, getProduct } from '../../service/home.js'
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    banners:[],
+    recommends:[]
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
   onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    //请求图片及数据
+    getMultiData().then(res => {
+      const banners = res.data.banner.list;
+      const recommends = res.data.recommend.list;
+      
+      //将banners和recommends放到data中
+      this.setData({
+        banners:banners,
+        recommends:recommends
+      })
+    })
   }
 })
---
+++ service/config.js
@@ -0,0 +1,3 @@
+//export const baseURL = "http://123.207.32.32:8000/api/hy"
+export const baseURL = 'http://106.54.54.237:8000/api/hy'
+export const timeout = 5000;
