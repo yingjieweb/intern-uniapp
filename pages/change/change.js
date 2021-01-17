8fb78185fee665d9f0f5caa02804606cb0d6e34f
yingjieweb <1184061078@qq.com>
2021-01-17 21:37:25 +0800

    basic implement change page

--- pages/change/change.js
+++ pages/change/change.js
@@ -1,66 +1,20 @@
-// pages/change/change.js
+//logs.js
+const util = require('../../utils/util.js')
+const app = getApp()
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    curLang: {},
+    langList: app.globalData.langList
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
   onShow: function () {
-
+    this.setData({ curLang: app.globalData.curLang })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  onTapItem: function(e) {
+    let langObj = e.currentTarget.dataset
+    wx.setStorageSync('language', langObj)
+    this.setData({'curLang': langObj})
+    app.globalData.curLang = langObj
+    wx.switchTab({ url: '/pages/index/index'})
   }
 })
